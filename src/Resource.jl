5a196442667782cf113b086099cc2550d64a55e9
Andrew Hale <andrew.hale003@gmail.com>
2023-01-28 16:26:54 +1000

    swagger added

--- src/Resource.jl
+++ src/Resource.jl
@@ -1,31 +1,95 @@
 module Resource
 
-using HTTP, JSON3
+using HTTP, JSON3, SwagUI, SwaggerMarkdown
 using ..Service, ..Model
 
 const ROUTER = HTTP.Router()
 
+# routes
+# HTTP.register!(ROUTER, "GET", "/", health_check)
+# HTTP.register!(ROUTER, "GET", "/generate", generate_abn)
+# HTTP.register!(ROUTER, "POST", "/acn-to-abn", acn_to_abn)
+# HTTP.register!(ROUTER, "POST", "/validate", validate_abn)
+# HTTP.register!(ROUTER, "GET", "/docs", docs)
+
+@swagger """
+/:
+    get:
+        description: Health Check
+        tags:
+            - 'Health Check'
+        responses:
+            '200': 
+                description: Check if the API is up
 """
-Health check endpoint for the API
-"""
-health_check(req) = "ok!"
-
-generate_abn(req) = HTTP.Response(200, JSON3.write(Service.generate_abn()))
-
-function acn_to_abn(req)
-    business = JSON3.read(req.body, AustralianBusiness)
-    return HTTP.Response(200, JSON3.write(Service.acn_to_abn(business.acn)))
-end
-
-function validate_abn(req)
-    business = JSON3.read(req.body, AustralianBusiness)
-    return HTTP.Response(200, JSON3.write(Service.validate_abn(business.abn)))
-end
-
+health_check(req) = HTTP.Response(200, JSON3.write((; msg="ok!")))
 HTTP.register!(ROUTER, "GET", "/", health_check)
+
+@swagger """
+/generate:
+    get:
+        description: Generate Australian Business Number
+        tags:
+            - 'Australian Business Number'
+        responses:
+            '200': 
+                description: Generate an Australian Business Number
+"""
+generate_abn(req) = HTTP.Response(200, JSON3.write(Service.generate_abn()))
 HTTP.register!(ROUTER, "GET", "/generate", generate_abn)
+
+@swagger """
+/acn-to-abn:
+    post:
+        description: Convert Australia Company Number
+        tags:
+            - 'Australian Business Number'
+        responses:
+            '200': 
+                description: Convert an Australia Company Number to an Australia Business Number
+            '400':
+                description: Invalid Australia Company Number
+"""
+function acn_to_abn(req)
+    try
+        business = JSON3.read(req.body, AustralianBusiness)
+        return HTTP.Response(200, JSON3.write(Service.acn_to_abn(business.acn)))
+    catch e
+        return HTTP.Response(400, "Error: $e")
+    end
+end
 HTTP.register!(ROUTER, "POST", "/acn-to-abn", acn_to_abn)
+
+@swagger """
+/validate:
+    post:
+        description: Validate an Australian Business Number
+        tags:
+            - 'Australian Business Number'
+        responses:
+            '200': 
+                description: Validate an Australian Business Number
+            '400':
+                description: Invalid Australian Business Number
+"""
+function validate_abn(req)
+    try
+        business = JSON3.read(req.body, AustralianBusiness)
+        return HTTP.Response(200, JSON3.write(Service.validate_abn(business.abn)))
+    catch e
+        return HTTP.Response(400, "Error: $e")
+    end
+end
 HTTP.register!(ROUTER, "POST", "/validate", validate_abn)
+
+# build swagger
+info = Dict{String,Any}()
+info["title"] = "Australian Business Number Service"
+info["version"] = "1.0.0"
+openApi = OpenAPI("2.0", info)
+swagger_document = build(openApi)
+docs(req) = HTTP.Response(200, render_swagger(swagger_document))
+HTTP.register!(ROUTER, "GET", "/docs", docs)
 
 function run()
     HTTP.serve(ROUTER, "0.0.0.0", 9111)
--- test/Client.jl
+++ test/Client.jl
@@ -1,17 +1,21 @@
-using Test, ABN, JSON3
+using ABN
+using Test, JSON3, HTTP
 
 server = @async ABN.run()
 
 # test server is up
-@test String(Client.health_check().body) == "ok!"
+@test JSON3.read(Client.health_check().body).msg == "ok!"
 
-# test abn generated
+# test generate
 abn = JSON3.read(Client.generate_abn().body)
 @test isinteger(abn)
 
-# test abn was valid
+# test validate
 @test JSON3.read(Client.validate_abn(abn).body)
+@test_throws HTTP.Exceptions.StatusError Client.validate_abn(1)
+@test_throws HTTP.Exceptions.StatusError Client.validate_abn("1")
 
+# test acn_to_abn
 # test an acn made from an abn is valid
 # if the 3rd digit of the abn is "0" it can not be used to make an acn, hardcoded one is used in instead
 acn = abn % (10^9) >= 100_000_000 ? abn % (10^9) : 801471421
@@ -20,3 +24,6 @@ abn_from_acn = JSON3.read(Client.acn_to_abn(acn).body)
 
 # the or condition is due to the 3rd digit issue
 @test abn_from_acn == abn skip = (acn == 801471421)
+
+@test_throws HTTP.Exceptions.StatusError JSON3.read(Client.acn_to_abn(1))
+@test_throws HTTP.Exceptions.StatusError JSON3.read(Client.acn_to_abn("1"))
